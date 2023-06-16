46facf4bb6cb606fc49d929cf4b03f39ff7e77d6
Navygeta <tinashenavonika@gmail.com>
2023-06-17 00:35:33 +0200

    3-print_alphabet commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,19 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char low[26] = "abcdefghijklmnopqrstuvwxyz";
+	char up[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+	int j;
 
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
+	for (j = 0; j < 26; j++)
+        {
+                putchar(up[j]);
+        }
 	putchar('\n');
 	return (0);
 }
