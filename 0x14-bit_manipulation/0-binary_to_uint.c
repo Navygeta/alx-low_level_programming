e3cef79941ff66f053a2a8c3ed7da255026ccc0d
Navygeta <tinashenavonika@gmail.com>
2023-08-03 20:01:35 +0200

    binary to unit updt3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int j;
-	unsigned int dec_val = 0;
-
-	if (!b)
+	if (b==NULL)
 		return (0);
 
-	for (j = 0; b[j]; i++)
+	char ch;
+	unsigned int dec_val = 0;
+
+	while ((ch = *b) != '\0')
 	{
-		if (b[j] < '0' || b[j] > '1')
+		if (ch != '0' && ch != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[j] - '0');
+		dec_val = 2 * dec_val + (ch - '0');
 	}
 
 	return (dec_val);
