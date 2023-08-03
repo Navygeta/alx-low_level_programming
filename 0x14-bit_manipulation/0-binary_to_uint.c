8a98c9018fbba8244604db11748bc056db6bf015
Navygeta <tinashenavonika@gmail.com>
2023-08-04 01:53:05 +0200

    binary to unit updtd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,19 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (b==NULL)
+	int i;
+	unsigned int result = 0;
+
+	if (!b)
 		return (0);
 
-	char ch;
-	unsigned int dec_val = 0;
-
-	while ((ch = *b) != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		if (ch != '0' && ch != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (ch - '0');
-		b++;
+		result = 2 * result + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return (result);
 }
