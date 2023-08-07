210bba5ea298bccd0da634e969e747e41215fc96
Navygeta <tinashenavonika@gmail.com>
2023-08-08 01:47:42 +0200

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,18 +11,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t file_descr;
+	ssize_t file_desc;
 	ssize_t bytes_r;
 	ssize_t bytes_wr;
 
-	file_descr = open(filename, O_RDONLY);
-	if (file_descr == -1)
+	file_desc = open(filename, O_RDONLY);
+	if (file_desc == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	bytes_r = read(file_descr, buffer, letters);
+	bytes_r = read(file_desc, buffer, letters);
 	bytes_wr = write(STDOUT_FILENO, buffer, bytes_r);
 
 	free(buffer);
-	close(file_descr);
+	close(file_desc);
 	return (bytes_wr);
 }
