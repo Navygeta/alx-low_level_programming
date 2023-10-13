80fc1190dbb75e5b640f0eff197de39ab65e6dab
Navygeta~ <tinashenavonika@gmail.com>
2023-10-13 22:44:30 +0200

    insert dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,11 +18,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -31,7 +29,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	temp = *h;
 	for ( ; i < idx - 1 && temp; i++)
 	{
@@ -42,13 +39,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = temp->next;
 	new_node->prev = temp;
-
 	if (temp->next)
 		temp->next->prev = new_node;
 	temp->next = new_node;
-
 	return (new_node);
 }
