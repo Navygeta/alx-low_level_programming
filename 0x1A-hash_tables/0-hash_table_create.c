6c281dc185c5d00ff817f94e66e6db0c560e040a
Navygeta <tinashenavonika@gmail.com>
2023-11-04 00:43:50 +0200

    hash create 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_ht = NULL;
 	unsigned long int i;
 
-	if (size < 1)
-	{
-		return (NULL);
-	}
-
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
 	{
 		return (NULL);
+	}
+
+	if (size < 1)
+	{
+		return (NULL)
 	}
 
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
