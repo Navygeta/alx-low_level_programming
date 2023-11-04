afda4888dd5077b1c77c7c74309ddcdf9e57e399
Navygeta <tinashenavonika@gmail.com>
2023-11-04 02:57:13 +0200

    print table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that prints a hash table
+ * @ht: Table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (unsigned long int i = 0; i < ht->size; i++)
+	{
+		hash_node_t *c_node = ht->array[i];
+
+		while (c_node != NULL)
+		{
+			printf("'%s': '%s'", c_node->key, c_node->value);
+			if (c_node->next != NULL)
+				printf(", ");
+			c_node = c_node->next;
+		}
+
+		if (i < ht->size - 1 && ht->array[i + 1] != NULL)
+			printf(", ");
+	}
+
+	printf("}\n");
+}
